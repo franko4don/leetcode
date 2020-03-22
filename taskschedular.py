20849941509d09f8d61c1be6c99872d78d81ef25
franko4don <franko4don@gmail.com>
2020-03-22 21:33:04 +0100

    more

---
+++ taskschedular.py
@@ -0,0 +1,18 @@
+class Solution:
+    def leastInterval(self, tasks, n) -> int:
+        character = [0 for i in range(26)]
+
+        for i in tasks:
+            character[ord(i) - ord('A')] += 1
+        character.sort()
+        max_value = character[len(character) - 1] - 1
+        idle_slots = max_value * n
+        for i in range(24, -1, -1):
+            idle_slots -= min(max_value, character[i])
+
+        return idle_slots + len(tasks) if idle_slots > 0 else len(tasks)
+
+m = Solution()
+print(m.leastInterval(["A", "A", "A", "B", "B", "B"], 2))
+
+print(ord('C') - ord('A'))
